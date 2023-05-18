133a26af428e4e52eef4074398ec15432bd8d2a6
Kay Knöpfle <35371434+Joystick01@users.noreply.github.com>
2023-05-18 13:47:22 +0200

    added backend connectivity to dev template

--- Component-Template-tailwind/src/lib/Component.svelte
+++ Component-Template-tailwind/src/lib/Component.svelte
@@ -15,7 +15,9 @@
 
   export const setStyle = () => {
     // inject css in shadow root manually (we done using <style>, Svelte removes unsed css selectors)
+    console.log(elem);
     const root = elem?.getRootNode();
+    console.log(root);
     if (root) {
       root.querySelectorAll('style').forEach((style) => root.removeChild(style));
       const style = document.createElement('style');
--- Component-dev-template/src/App.svelte
+++ Component-dev-template/src/App.svelte
@@ -1,13 +1,32 @@
 <script>
-  import Component from './lib/Component.svelte'
+	import { onMount } from "svelte";
+	import Component from "./lib/Component.svelte";
+	import { connectSocket } from "./socket";
+
+	let container;
+
+	async function login(){
+		let auth = await fetch(`http://${import.meta.env.VITE_API_URI}/auth`, {
+            method: "POST",
+            body: JSON.stringify({
+                method: "PAM",
+                credentials: {
+                    username: import.meta.env.VITE_USERNAME,
+                    password: import.meta.env.VITE_PASSWORD
+                }
+            })
+        })
+		let json = await auth.json();
+		console.log(`TOKEN: ${json.access_token}`);
+		connectSocket(`ws://${import.meta.env.VITE_API_URI}/ws?token=${json.access_token}`, () => {
+			const child = document.createElement(import.meta.env.VITE_HTMLTAG);
+			container.appendChild(child);
+		})
+	}
+	
+	onMount(() => {
+		login()
+	});
 </script>
 
-<main>
-
-    <counter-test />
-
-</main>
-
-<style>
-
-</style>
+<div bind:this={container}/>
